40a042d868c58c7e3352ece7a7cb328e9d540f99
Emil Gräs <emgr02002@utb.vaxjo.se>
2019-11-28 10:58:36 +0100

    Metoder uppgift 2

---
+++ Metoder/Metoder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Metoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("10 upphöjt till 7: " + Power(10, 7));
+            Console.WriteLine("2 upphöjt till 10: " + Power(2, 10));
+
+        }
+        private static float Power(float a, int b)
+        {
+            float w = a;
+            for (int i = 1; i < b; i++)
+            {
+                w = a * w;
+
+            }
+            return w;
+        }
+
+
+    }
+}
